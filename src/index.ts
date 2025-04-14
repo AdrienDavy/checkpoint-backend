eae538fcc01fe06d123a4035e35eaa3fc8c11206
Adrien Davy <adriendavyfx@gmail.com>
2025-04-14 09:27:19 +0200

    Added SQLite data source configuration and application initialization

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello, world!");
+import "reflect-metadata";
+import { datasource } from "./datasource";
+
+async function initialize() {
+  await datasource.initialize();
+  console.info("Datasource is connected 🔌");
+}
+
+initialize();
